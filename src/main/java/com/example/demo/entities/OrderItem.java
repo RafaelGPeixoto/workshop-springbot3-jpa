8882110e771430b358b70a8a6a60b501b9468411
Rafael Peixoto <rafagonca97@gmail.com>
2026-01-07 00:00:35 -0300

    Subtotal & Total methods

--- src/main/java/com/example/demo/entities/OrderItem.java
+++ src/main/java/com/example/demo/entities/OrderItem.java
@@ -40,7 +40,7 @@ public class OrderItem implements Serializable {
 	public void setOrder(Order order) {
 		id.setOrder(order);
 	}
-	 
+	  
 	public Product getOProducts() {
 		return id.getProduct();
 	}
@@ -64,6 +64,10 @@ public class OrderItem implements Serializable {
 
 	public void setPrice(Double price) {
 		this.price = price;
+	}
+	
+	public Double getSubTotal() {
+		return price * quantity;
 	}
 
 	@Override
