570c67bd7304eb2e08f9bb0e7b7ab2eb31af3376
Rafael Peixoto <rafagonca97@gmail.com>
2026-01-12 00:17:37 -0300

    User insert

--- src/main/java/com/example/demo/resources/CategoryResource.java
+++ src/main/java/com/example/demo/resources/CategoryResource.java
@@ -9,27 +9,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.entities.User;
+import com.example.demo.entities.Category;
+import com.example.demo.services.CategoryService;
 import com.example.demo.services.UserService;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value = "/categories")
 public class CategoryResource {
 	
 	@Autowired
-	private UserService service;
+	private CategoryService service;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
-		List<User> list = service.findALL();
+	public ResponseEntity<List<Category>> findAll(){
+		List<Category> list = service.findALL();
 		
 		return ResponseEntity.ok().body(list);	
 		
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id){
-		User obj = service.findById(id);
+	public ResponseEntity<Category> findById(@PathVariable Long id){
+		Category obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
 	
